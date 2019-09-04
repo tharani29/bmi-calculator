9dca7d08c6d63ab15dc2283ebe2f2845a863d120
GermaVinsmoke <vaibhav1180@gmail.com>
2019-09-05 00:05:26 +0530

    Updates

---
+++ precache-manifest.6aa5d1f1520a00f78385eb29ba5ac471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9322ead244147b830a415e3497b44b8",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "c69df26c930a0f1c6e23",
+    "url": "/bmi-calculator/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "2a62e0f4f2793f04ff29",
+    "url": "/bmi-calculator/static/css/main.6c8d8651.chunk.css"
+  },
+  {
+    "revision": "c69df26c930a0f1c6e23",
+    "url": "/bmi-calculator/static/js/2.4afff688.chunk.js"
+  },
+  {
+    "revision": "2a62e0f4f2793f04ff29",
+    "url": "/bmi-calculator/static/js/main.ceea8e30.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
